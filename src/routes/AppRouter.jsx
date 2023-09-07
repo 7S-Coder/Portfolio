53bbea51143acbf63bfca9c5a160cf80b3ec28f4
7S-Coder <dylan.e33@hotmail.fr>
2023-09-07 03:04:40 +0200

    add routes folder

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+
+const AppRouter = () => {
+  return (
+    <Routes>
+      {/* <Route path="/" element={<Home />} /> */}
+      {/* <Route path="*" element={<Error404 />} /> */}
+    </Routes>
+  );
+};
+
+export default AppRouter;
