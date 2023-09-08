434fac03a1d4f9fffb319d557c7052daf83865e0
7S-Coder <dylan.e33@hotmail.fr>
2023-09-08 03:27:56 +0200

    ended home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
+import "../styles/pages/Home.scss";
+
 const Home = () => {
   return (
-    <>
-      <h1>Bienvenue sur la maison</h1>
-    </>
+    <div className="container">
+      <section>
+        <h1>
+          Bienvenue <br />
+          sur le portfolio de <span>SEPT</span>
+        </h1>
+        <p>Développeur web frontend passioné par javascript</p>
+      </section>
+    </div>
   );
 };
 
