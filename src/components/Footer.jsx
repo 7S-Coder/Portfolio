07c042218d2474f28b787ddf110c926beabb4a37
7S-Coder <dylan.e33@hotmail.fr>
2023-09-10 10:45:02 +0200

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,14 @@
+import { Link } from "react-router-dom";
 import "../styles/components/Footer.scss";
 
 const Footer = () => {
   return (
     <footer>
       <div>
-        <a href="https://github.com/7S-Coder" target="blank">
+        <Link to="https://github.com/7S-Coder" target="blank">
           <img src="src\assets\svg\github.svg" alt="logo Github" />
           7S-Coder
-        </a>
-
-        <span> Powered by Sept.</span>
+        </Link>
       </div>
     </footer>
   );
